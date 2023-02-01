cfdb444e10ed84636be3535d3ffcf2dc458a54a6
hiepph <bm.hcmc.hiep@modetour.com>
2023-02-01 08:03:07 +0700

    add nuxt

---
+++ pages/test.vue
@@ -0,0 +1,26 @@
+<template>
+  <h1 class=" text-red-400">{{ count }}</h1>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup() {
+    const count = ref(5)
+
+    // expose to template and other options API hooks
+    return {
+      count
+    }
+  },
+
+  mounted() {
+    console.log(this.count) // 0
+  }
+}
+</script>
+
+<style>
+
+</style>
